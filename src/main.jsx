8c62b2dc75c81d65d96558674022a1ec438ff170
Diego <alfiandrodiego@gmail.com>
2022-11-20 10:24:47 +0700

    Memperbaiki kesalahan di main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from "react-router-dom";
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
